48067b84ebf36bdf4873a08ee2228a8184a79758
MartinBastias <mbastias2005@gmail.com>
2026-05-01 04:23:53 -0400

    terminar subclase Moneda500

---
+++ src/monedas/Moneda500.java
@@ -0,0 +1,25 @@
+package monedas;
+
+/**
+ * clase de moneda de 500, hereda de Moneda
+ */
+public class Moneda500 extends Moneda{
+
+    /**
+     * constructor de la clase Moneda500
+     * inicializa la moneda de 500 llamando a la clase padre Moneda
+     */
+    public Moneda500(){
+        super();
+    }
+
+    /**
+     * obtiene valor de la moneda
+     *
+     * @return valor de la moneda(100)
+     */
+    @Override
+    public int getValor(){
+        return 500;
+    }
+}
