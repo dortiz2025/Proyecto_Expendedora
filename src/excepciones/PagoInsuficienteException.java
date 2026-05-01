97993b1e53e65f96c893833453752f4fa6f9d84b
MartinBastias <mbastias2005@gmail.com>
2026-05-01 17:43:18 -0400

    Documentacion PagoInsuficienteException

--- src/excepciones/PagoInsuficienteException.java
+++ src/excepciones/PagoInsuficienteException.java
@@ -5,6 +5,9 @@ package excepciones;
  */
 public class PagoInsuficienteException extends Exception {
     /**
+     * Constructor de la excepción
+     * Crea una instancia con una advertencia del error
+     *
      * @param mensaje Entrega razón de la excepción
      */
     public PagoInsuficienteException(String mensaje) {
