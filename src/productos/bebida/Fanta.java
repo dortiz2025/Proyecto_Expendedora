1fd548d5561a5b08d03c265d513e1f4ce9a1878a
MartinBastias <mbastias2005@gmail.com>
2026-05-01 17:06:50 -0400

    Documentacion Fanta

--- src/productos/bebida/Fanta.java
+++ src/productos/bebida/Fanta.java
@@ -1,15 +1,25 @@
 package productos.bebida;
 
+/**
+ * Clase que representa una Fanta
+ * Hereda de su clase padre Bebida
+ */
 public class Fanta extends Bebida {
     /**
+     * Constructor de la bebida Fanta
      * Asigna identificador de la bebida
      *
      * @param serie Identificador de la Bebida
      */
     public Fanta(int serie) {
         super(serie);
-    };
+    }
 
+    /**
+     * Simula tomarse la Fanta
+     *
+     * @return Un String con el sabor de la bebida ("Fanta")
+     */
     @Override
     public String consumir() {
         return "Fanta";
