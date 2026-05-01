28f18aec7209f5c3389a7012664f6d550520d43e
MartinBastias <mbastias2005@gmail.com>
2026-05-01 04:31:30 -0400

    corregir comentarios de constructor y metodos en Moneda1000

--- src/monedas/Moneda1000.java
+++ src/monedas/Moneda1000.java
@@ -4,13 +4,19 @@ package monedas;
  * clase de moneda de 1000, hereda de Moneda
  */
 public class Moneda1000 extends Moneda{
+
+    /**
+     * constructor de la clase Moneda1000
+     * inicializa la moneda de 1000 llamando a la clase padre Moneda
+     */
     public Moneda1000(){
         super();
     }
 
     /**
-     * constructor de la clase Moneda1000
-     * inicializa la moneda de 1000 llamando a la clase padre Moneda
+     * obtiene valor de la moneda
+     *
+     * @return valor de la moneda(1000)
      */
     @Override
     public int getValor(){
