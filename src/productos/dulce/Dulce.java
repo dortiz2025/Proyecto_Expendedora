0e89fb3e4d29f69a94045a49588abea182b29eb8
MartinBastias <mbastias2005@gmail.com>
2026-05-01 17:07:22 -0400

    Documentacion Dulce

--- src/productos/dulce/Dulce.java
+++ src/productos/dulce/Dulce.java
@@ -2,8 +2,13 @@ package productos.dulce;
 
 import productos.Producto;
 
+/**
+ * Clase abstracta que representa un Producto de tipo Dulce.
+ * Es la base para los dulces que hay en la expendedora
+ */
 public abstract class Dulce extends Producto {
     /**
+     * Constructor del dulce.
      * Asigna identificador del Dulce
      *
      * @param serie Identificador del Dulce
