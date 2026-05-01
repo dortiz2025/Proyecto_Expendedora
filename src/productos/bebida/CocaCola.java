79c704e237ae4ee342f23e3d928fc5ef0ca4689c
MartinBastias <mbastias2005@gmail.com>
2026-05-01 17:00:01 -0400

    Documentacion CocaCola

--- src/productos/bebida/CocaCola.java
+++ src/productos/bebida/CocaCola.java
@@ -1,7 +1,12 @@
 package productos.bebida;
 
+/**
+ * Clase que representa un producto de tipo CocaCola
+ * Hereda de la clase abstracta Bebida
+ */
 public class CocaCola extends Bebida {
     /**
+     * Constructor de la bebida CocaCola
      * Asigna identificador de la bebida
      *
      * @param serie Identificador de la Bebida
@@ -10,6 +15,11 @@ public class CocaCola extends Bebida {
         super(serie);
     };
 
+    /**
+     * Simula el tomarse la bebida
+     *
+     * @return Un String con el sabor ("CocaCola")
+     */
     @Override
     public String consumir(){
         return "CocaCola";
