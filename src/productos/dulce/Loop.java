6243cdb1033dc46eb64619388fa9ab2dadc75992
MartinBastias <mbastias2005@gmail.com>
2026-05-01 17:26:45 -0400

    Documentacion de Loop

--- src/productos/dulce/Loop.java
+++ src/productos/dulce/Loop.java
@@ -1,7 +1,12 @@
 package productos.dulce;
 
+/**
+ * Clase de un Producto de tipo Loop
+ * Hereda de su padre Dulce
+ */
 public class Loop extends Dulce {
     /**
+     * Constructor del Loop
      * Asigna identificador del Dulce
      *
      * @param serie Identificador del Dulce
@@ -10,6 +15,11 @@ public class Loop extends Dulce {
         super(serie);
     }
 
+    /**
+     * Simula comerse un Loop
+     *
+     * @return Un String con el nombre del dulce ("Loop)
+     */
     @Override
     public String consumir(){
         return "Loop";
