ac1f2e654f29d36d27919cf37807facfacd3387c
dortiz2025 <dortiz2025@inf.udec.com>
2026-05-02 01:17:53 -0400

    Main Interactivo Listo

--- src/logica/MainInteractivo.java
+++ src/logica/MainInteractivo.java
@@ -37,7 +37,7 @@ public class MainInteractivo {
 
             //Se pide el input
             System.out.println("0. Salir");
-            System.out.println("Ingrese el número del producto que desea comprar.");
+            System.out.print("Ingrese el número del producto que desea comprar: ");
 
             //Se asume que el input es entero
             int opcion = sc.nextInt();
@@ -47,29 +47,66 @@ public class MainInteractivo {
                 System.out.println("Saliendo del Expendedor Interactivo");
                 break; //Termina el programa.
             }
-            try {
-                // Verificamos que el número esté dentro del rango
-                if (opcion < 1 || opcion > productos.length) {
-                    System.out.println("Opción inválida. Intente de nuevo.");
-                    continue; // Vuelve al inicio del bucle
-                }
 
+            // Verificamos que el número esté dentro del rango
+            if (opcion < 1 || opcion > productos.length) {
+                System.out.println("Opción inválida. Intente de nuevo.");
+                continue; // Vuelve al inicio del bucle
+            }
+
+            System.out.println();//Estética
+            System.out.println("Seleccione la moneda con la que va a pagar:");
+            System.out.println("1. Moneda de $100");
+            System.out.println("2. Moneda de $500");
+            System.out.println("3. Moneda de $1000");
+            System.out.println("4. Moneda de $1500");
+            System.out.println("5. Moneda falsa");
+            System.out.print("\nIngrese su opción de pago: ");
+
+            //Recibe input de moneda elegida
+            int opcionMoneda = sc.nextInt();
+
+            Moneda monedaDePago; // Inicia como null por defecto
+
+            // Asignamos la instancia correspondiente según la opción
+            switch(opcionMoneda) {
+                case 1:
+                    monedaDePago = new Moneda100();
+                    break;
+                case 2:
+                    monedaDePago = new Moneda500();
+                    break;
+                case 3:
+                    monedaDePago = new Moneda1000();
+                    break;
+                case 4:
+                    monedaDePago = new Moneda1500();
+                    break;
+                case 5:
+                    monedaDePago = null; //Se puede intentar comprar sin moneda
+                    break;
+                default:
+                    System.out.println("Opción de moneda no válida. No hay moneda.");
+                    monedaDePago = null;
+                    break;
+            }
+
+            try {
                 // Transformamos el input a TipoProducto (uso de enums)
                 TipoProducto productoElegido = productos[opcion - 1];
 
-
-
-                //Compramos
+                //Intentamos hacer una compra
                 Comprador comprador = new Comprador(monedaDePago, productoElegido, expendedor);
 
+                //Si se hace la compra mostramos que se consumió y si sobro vuelto;
                 System.out.println("¡Compra exitosa!");
                 System.out.println("Consumiste: " + comprador.queConsumiste());
                 System.out.println("Tu vuelto es: $" + comprador.cuantoVuelto());
 
-            } catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e) {
+            }
+            //Si hay una excepción, se reporta al usuario
+            catch (NoHayProductoException | PagoInsuficienteException | PagoIncorrectoException e) {
                 System.out.println("Problema con la compra: " + e.getMessage());
-            } catch (Exception e) {
-                System.out.println("Error inesperado: " + e.getMessage());
             }
         }
     }
