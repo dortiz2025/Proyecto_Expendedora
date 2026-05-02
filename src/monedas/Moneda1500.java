695c9da00f3ca9a5594495fd643107ce06412d92
MartinBastias <mbastias2005@gmail.com>
2026-05-01 23:20:15 -0400

    Agregar moneda 1500

---
+++ src/monedas/Moneda1500.java
@@ -0,0 +1,25 @@
+package monedas;
+
+/**
+ * Clase de moneda de 1500, hereda de Moneda
+ */
+public class Moneda1500 extends Moneda{
+
+    /**
+     * Constructor de la clase Moneda1500
+     * Inicializa la moneda de 1500 llamando a la clase padre Moneda
+     */
+    public Moneda1500(){
+        super();
+    }
+
+    /**
+     * Obtiene valor de la moneda
+     *
+     * @return Valor de la moneda(1500)
+     */
+    @Override
+    public int getValor(){
+        return 1500;
+    }
+}
